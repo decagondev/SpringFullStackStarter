e1c45d9817d3c75afe06cf194e8da59858ef0abb
Tom Tarpey <tomtarpeydev@gmail.com>
2022-09-13 12:28:31 +0100

    initial commit

---
+++ src/main/java/com/decadev/ApibeApplication.java
@@ -0,0 +1,13 @@
+package com.decadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibeApplication.class, args);
+	}
+
+}
